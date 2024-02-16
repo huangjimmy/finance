5a1916afb82d8df9700c65a968839b16d3fb3508
Jimmy Huang <jimmy.s.huang@gmail.com>
2024-02-16 08:59:59 -0800

    modify scripts/buiid.sh

--- scripts/build.sh
+++ scripts/build.sh
@@ -1,7 +1,7 @@
 git clone https://github.com/asdf-vm/asdf.git ~/.asdf --branch v0.14.0
-alias asdf="$HOME/.asdf/asdf.sh"
-asdf plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
-asdf install
+chmod +x ~/.asdf/asdf.sh
+~/.asdf/asdf.sh plugin add nodejs https://github.com/asdf-vm/asdf-nodejs.git
+~/.asdf/asdf.sh install
 npm install
 ./gradlew -i clean
 ./gradlew -i update
